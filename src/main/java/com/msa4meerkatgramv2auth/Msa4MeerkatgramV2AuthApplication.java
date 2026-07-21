86ec81072ef1a3f521f32cf84ba326534cbca3d1
ByungjooPark <milkpbj23@gmail.com>
2026-07-21 18:02:29 +0900

    add login & security

---
+++ src/main/java/com/msa4meerkatgramv2auth/Msa4MeerkatgramV2AuthApplication.java
@@ -0,0 +1,13 @@
+package com.msa4meerkatgramv2auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa4MeerkatgramV2AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Msa4MeerkatgramV2AuthApplication.class, args);
+    }
+
+}

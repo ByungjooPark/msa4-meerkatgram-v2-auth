a4ef4b5298f5ddc23346a8afee0c897e0ee000b7
ByungjooPark <milkpbj23@gmail.com>
2026-07-22 16:12:25 +0900

    add login & security & cookie & jwt complete

--- src/main/java/com/msa4meerkatgramv2auth/Msa4MeerkatgramV2AuthApplication.java
+++ src/main/java/com/msa4meerkatgramv2auth/Msa4MeerkatgramV2AuthApplication.java
@@ -2,8 +2,12 @@ package com.msa4meerkatgramv2auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableJpaAuditing
 public class Msa4MeerkatgramV2AuthApplication {
 
     public static void main(String[] args) {
